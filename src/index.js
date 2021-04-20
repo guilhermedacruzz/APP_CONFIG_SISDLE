8fb292907bf80e74b8191a4efb66c228d15eea13
indexGuilherme <gddcruz04@gmail.com>
2021-04-20 14:28:28 -0300

    Começando o projeto

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import { View, ImageBackground, Text, StyleSheet, StatusBar, ScrollView} from 'react-native';
+ 
+export default function App() {
+
+    return (
+        <View>
+            <Text> sdjkfd</Text>
+        </View>
+    );
+}
