39b0d9bba440beddf710298f9eb4ae906f1e270f
indexGuilherme <gddcruz04@gmail.com>
2021-04-25 10:33:49 -0300

    Pedindo permissão de localização

--- src/components/Pressable/index.js
+++ src/components/Pressable/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, Pressable} from 'react-native';
+
+import {verifyLocationPermission} from '../../utils/permission';
 
 import styles from './styles';
 import colors from '../../styles/colors';
 
 export default function(props) {
+
+    useEffect(() => {
+        async function check() {
+            let a = verifyLocationPermission();
+            if(a) {
+                console.log("Sim");
+            }
+            else {
+                console.log("nao");
+            }
+
+        };
+
+        if(props.hasPressable == "Pressionado")
+            check();
+    }, [props.hasPressable]);
 
     return(
         <View>
@@ -16,7 +34,6 @@ export default function(props) {
                     },
                     styles.sectionButton
                 ]}
-                
             >
                 <Text style={styles.sectionButtonText}>Enviar</Text>
             </Pressable>
--- src/utils/permission.js
+++ src/utils/permission.js
@@ -1,15 +1,13 @@
-import React from 'react';
-
 import {PermissionsAndroid} from 'react-native';
 
-
-export default async function verifyLocationPermission() {
+export async function verifyLocationPermission() {
     try {
         const granted = await PermissionsAndroid.request(
             PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
         );
         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
             console.log('permissão concedida');
+            return true;
         } else {
             console.error('permissão negada');
             return false;
